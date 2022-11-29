5d9c80837ef5d4660a61447d3a11821060d78ad0
Josh Archer <jarcher@greenriver.edu>
2022-11-29 13:39:54 -0800

    Added initial files.

---
+++ src/main/java/com/example/f22circleci/F22CircleCiApplication.java
@@ -0,0 +1,15 @@
+package com.example.f22circleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F22CircleCiApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(F22CircleCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/f22circleci/F22CircleCiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.f22circleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F22CircleCiApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
